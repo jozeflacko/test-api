771ba7e95e995f02bdb85bee6d3b93ed28e516e4
Jozef Lacko <jlacko27@gmail.com>
2021-02-13 20:34:51 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.AttributeName = void 0;
+var AttributeName;
+(function (AttributeName) {
+    AttributeName["id"] = "id";
+    AttributeName["className"] = "className";
+    AttributeName["href"] = "href";
+    AttributeName["value"] = "value";
+})(AttributeName = exports.AttributeName || (exports.AttributeName = {}));
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+export enum AttributeName {
+    id= 'id',
+    className='className',
+    href='href',
+    value='value',
+}
+
+export default interface Test {
+
+    querySelectorAll(querySelector: string): unknown;
+
+    assertExists(): void;
+
+    assertNotExists(): void;
+
+    assertIsDisabled(): void;
+
+    assertIsActive(): void;
+
+    assertHasText(text: string): void;
+
+    click(): void;
+
+    write(value: string | number): void;
+
+    assertHasAttribute(attributeName: AttributeName, attributeValue: string): void;
+
+    assertHasNotAttribute(attributeName: AttributeName, attributeValue: string): void;
+}
